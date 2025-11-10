80d4ccc5ce58e098f39f6bad34ef3c616b69e527
Steven <sbjones.1776@gmail.com>
2025-11-10 15:47:06 -0600

    update

--- app/src/main/java/app/web/math_adventure_app/twa/DelegationService.java
+++ app/src/main/java/app/web/math_adventure_app/twa/DelegationService.java
@@ -1,18 +1,12 @@
 package app.web.math_adventure_app.twa;
 
 
-import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
-
-
 public class DelegationService extends
         com.google.androidbrowserhelper.trusted.DelegationService {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        
-            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
-        
+        // Billing library removed - using web-based payments only
     }
 }
 
